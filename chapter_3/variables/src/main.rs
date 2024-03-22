6cb9b7bb184d8f2518886e713d7cbe570cec86fc
Jonathan Lindquist <26193336+jonathanLindquist@users.noreply.github.com>
2024-03-22 16:04:13 -0500

    ch 3

---
+++ chapter_3/variables/src/main.rs
@@ -0,0 +1,28 @@
+fn main() {
+    // const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+
+    // println!("the const: {THREE_HOURS_IN_SECONDS}");
+
+    // let mut x = 5;
+    // println!("The value of x is: {x}");
+    // x = 6;
+    // println!("The value of x is: {x}");
+
+    let x = 5;
+
+    let x = x + 1;
+
+    {
+        let x = x * 2;
+        println!("the value of x in the inner scope is: {x}");
+    }
+
+    println!("The value of x is: {x}");
+
+    let spaces = "   ";
+    let spaces = spaces.len();
+
+    // compile-time error
+    // let mut spaces = "   ";
+    // spaces = spaces.len();
+}

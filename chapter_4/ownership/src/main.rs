e2ec260d39a702907bcea28e52f892bde377ba08
Jonathan Lindquist <26193336+jonathanLindquist@users.noreply.github.com>
2024-03-24 09:06:00 -0500

    ownership

---
+++ chapter_4/ownership/src/main.rs
@@ -0,0 +1,30 @@
+fn main() {
+    let s1 = String::from("Hello");
+    let s2 = s1;
+    
+    // error because s1 is NO LONGER valid, could duplicate heap with let s2 = s1.clone();
+    // println!("string values: {s1}, {s2}");
+
+    println!("string value: {s2}");
+
+    let s = String::from("hello");  // s comes into scope
+
+    takes_ownership(s);             // s's value moves into the function...
+                                    // ... and so is no longer valid here
+
+    let x = 5;                      // x comes into scope
+
+    makes_copy(x);                  // x would move into the function,
+                                    // but i32 is Copy, so it's okay to still
+                                    // use x afterward
+} // Here, x goes out of scope, then s. But because s's value was moved, nothing
+  // special happens.}
+
+fn takes_ownership(some_string: String) { // some_string comes into scope
+    println!("{}", some_string);
+} // Here, some_string goes out of scope and `drop` is called. The backing
+  // memory is freed.
+
+fn makes_copy(some_integer: i32) { // some_integer comes into scope
+    println!("{}", some_integer);
+}

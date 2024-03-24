5a578ddbcf3a4b5eaa25dd56bbdd1e3c796767d8
Jonathan Lindquist <26193336+jonathanLindquist@users.noreply.github.com>
2024-03-24 13:50:21 -0500

    ch4 references

---
+++ chapter_4/references/src/main.rs
@@ -0,0 +1,46 @@
+fn main() {
+    let s1 = String::from("Hello!");
+
+    let len = calculate_length(&s1);
+
+    // change(&s1);
+
+    println!("The length of '{}' is {}.", s1, len);
+
+    let mut s = String::from("World!");
+
+    change(&mut s);
+
+    {
+        // more that 1 mutable reference to the same variable will fail to compile
+        let mut_x = &mut s;
+        // let mut_y = &mut s;
+    }
+
+    let mut_y = &mut s; // this is fine since mut_x is no longer in scope
+
+    ////////////////////////////
+
+    let mut s50 = String::from("hello");
+
+    let r1 = &s50; // no problem
+    let r2 = &s50; // no problem
+    println!("{} and {}", r1, r2);
+    // variables r1 and r2 will not be used after this point
+
+    let r3 = &mut s50; // no problem
+    println!("{}", r3);
+}
+
+fn calculate_length(s: &String) -> usize { // s is a reference to a String
+    s.len()
+} // Here, s goes out of scope. But because it does not have ownership of what
+  // it refers to, it is not dropped.
+
+//   fn change(some_string: &String) {
+//     some_string.push_str(", world");
+// } // this function will fail
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
+} // this works
